704efd4156547eebe60683e3a68c4fbab0ed2369
Vinícius Freire <vini.freire10@gmail.com>
2020-12-01 19:32:04 -0300

    (refact)Corrige bug da pagina do acervo para telas grandes, padroniza videos width e corrige imagens footer

--- components/footer.js
+++ components/footer.js
@@ -11,12 +11,12 @@ customElements.define('footer-new', class extends HTMLElement {
       <div class="footer-center">
            <div class="social-icons">
                <h4 class="footer-text">Acompanhe nossas redes sociais:</h4>
-               <a href="https://www.facebook.com/museupraquem/" target="_blank"><img src='/assets/icons-social/facebook (1).svg' class="footer-image" /></a>
+               <a href="https://www.facebook.com/museupraquem/" target="_blank"><img src='assets\icons-social\facebook (1).svg' class="footer-image" /></a>
                <!-- 
                <a href="http://www.twitter.com"><img src='/assets/icons-social/twitter (1).svg' /></a>
                <a href="http://www.linkedin.com"><img src='/assets/icons-social/linkedin (1).svg' /></a> 
                !-->
-               <a href="https://instagram.com/museupraquem?igshid=wa0gzfkea5sk" target="_blank"> <img src="/assets/icons-social/instagram.png" class="footer-image" /> </a>
+               <a href="https://instagram.com/museupraquem?igshid=wa0gzfkea5sk" target="_blank"> <img src="assets\icons-social\instagram.png" class="footer-image" /> </a>
            </div>
       </div>
        
