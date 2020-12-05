3d91ddaa3c84f01fb878dbff3cfa192559da3d09
Vinícius Freire <vini.freire10@gmail.com>
2020-12-04 22:17:34 -0300

    (feat)Atualiza função de mostrar transcrição para alternar entre mostrar e minimizar

--- js/transcript.js
+++ js/transcript.js
@@ -1,6 +1,13 @@
 
 document.getElementById("transcricao").style.display = "none"
 
-function myFunction() {
-    document.getElementById("transcricao").style.display = "block"
+function toggleTranscript() {
+
+    if (document.getElementById("transcricao").style.display === "none"){
+        document.getElementById("transcricao").style.display = "block";
+        document.getElementById("btnTrcpt").innerText = "Minimizar a transcrição";
+    } else {
+        document.getElementById("transcricao").style.display = "none";
+        document.getElementById("btnTrcpt").innerText = "Ler a transcrição";
+    }; 
 }
